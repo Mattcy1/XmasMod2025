e94e18db8e659ad5ea74ec532388f3e9ee64ae15
liamw <liamwolfguy@gmail.com>
2025-10-19 15:34:22 -0400

    Gift upgrade stuff

--- Towers/XmasTowerSet.cs
+++ Towers/XmasTowerSet.cs
@@ -12,6 +12,7 @@ using Il2CppAssets.Scripts.Unity.Bridge;
 using Il2CppAssets.Scripts.Unity.UI_New.InGame;
 using Il2CppAssets.Scripts.Unity.UI_New.InGame.RightMenu;
 using Il2CppAssets.Scripts.Unity.UI_New.InGame.StoreMenu;
+using Il2CppAssets.Scripts.Unity.UI_New.InGame.TowerSelectionMenu;
 using MelonLoader;
 
 namespace XmasMod2025.Towers;
@@ -91,6 +92,92 @@ public class XmasTowerSet : ModTowerSet
                 if (InGame.instance != null || InGame.instance.bridge != null)
                 {
                     InGame.instance.bridge.simulation.CreateTextEffect(__instance.Position, ModContent.CreatePrefabReference<CollectText>(), 2f, $"+{__instance.SaleValue} Gifts", true);
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(UpgradeButton), nameof(UpgradeButton.GetUpgradeStatus))]
+    private static class UpgradeButton_CheckCash
+    {
+        public static bool Prefix(UpgradeButton __instance, ref UpgradeButton.UpgradeStatus __result)
+        {
+            if (!__instance || __instance.upgrade == null)
+            {
+                return true;
+            }
+            ModUpgrade? modUpgrade = GetContent<ModUpgrade>().Find(m => m.Id == __instance.upgrade.name);
+            if (modUpgrade != null && modUpgrade is ChristmasUpgrade cUpgrade && cUpgrade.CostsGifts)
+            {
+                if (XmasMod2025.Gifts < cUpgrade.Cost)
+                {
+                    __instance.upgradeStatus = UpgradeButton.UpgradeStatus.CanNotAfford;
+                    __result = UpgradeButton.UpgradeStatus.CanNotAfford;
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+    [HarmonyPatch(typeof(UpgradeButton), nameof(UpgradeButton.UpdateCostVisuals))]
+    private static class UpgradeButton_UpdateCostVisuals
+    {
+        public static void Postfix(UpgradeButton __instance)
+        {
+            if (!__instance || __instance.upgrade == null)
+            {
+                return;
+            }
+            ModUpgrade? modUpgrade = GetContent<ModUpgrade>().Find(m => m.Id == __instance.upgrade.name);
+            if (modUpgrade is ChristmasUpgrade { CostsGifts: true } cUpgrade)
+            {
+                __instance.cost.text = $"{cUpgrade.Cost} Gifts";
+            }
+        }
+    }
+    [HarmonyPatch(typeof(TowerManager), nameof(TowerManager.UpgradeTower))]
+    private static class TowerManager_UpgradeTower
+    {
+        [HarmonyPrefix]
+        public static void Prefix(ref Tower tower, ref TowerModel def, ref string __state)
+        {
+            __state = null;
+            foreach (var upgrade in def.appliedUpgrades)
+            {
+                if (def.appliedUpgrades == null) return;
+                if (!tower.towerModel.appliedUpgrades.Contains(upgrade))
+                {
+                    __state = upgrade;
+                    var unrefTower = tower;
+                    var unrefDef = def;
+                    tower = unrefTower;
+                    def = unrefDef;
+                }
+            }
+        }
+
+        [HarmonyPostfix]
+        public static void Postfix(Tower tower, TowerModel def, float upgradeCost, string __state)
+        {
+            if (__state != null)
+            {
+                var modTower = tower.towerModel.GetModTower();
+                if (modTower is ChristmasTower { CostsGifts: true })
+                {
+                    var modUpgrade =  GetContent<ModUpgrade>().Find(m => m.Id == __state);
+                    if (modUpgrade is ChristmasUpgrade { CostsGifts: true } cUpgrade)
+                    {
+                        tower.worth -= upgradeCost;
+                        tower.worth += cUpgrade.Cost;
+                        tower.GetTowerToSim().sim.AddCash(upgradeCost, Simulation.CashSource.TowerUpgraded);
+                        XmasMod2025.Gifts -= cUpgrade.Cost;
+                        
+                        if (InGame.instance != null || InGame.instance.bridge != null)
+                        {
+                            InGame.instance.bridge.simulation.CreateTextEffect(tower.Position, CreatePrefabReference<CollectText>(), 2f, $"-{cUpgrade.Cost} Gifts", true);
+                        }
+                    }
                 }
             }
         }
