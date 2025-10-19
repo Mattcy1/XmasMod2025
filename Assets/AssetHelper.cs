50f076a894dc094e35e9e332dfd55f5aa7065e04
liamw <liamwolfguy@gmail.com>
2025-10-19 12:21:58 -0400

    Better asset things

---
+++ Assets/AssetHelper.cs
@@ -0,0 +1,33 @@
+﻿using BTD_Mod_Helper.Api.Internal;
+using UnityEngine;
+
+namespace XmasMod2025.Assets;
+
+internal static class AssetHelper
+{
+    public static AssetBundle bundle;
+
+    public static GameObject GetObject(string name)
+    {   
+        return Get<GameObject>(name);
+    }
+
+    public static Sprite GetSprite(string name)
+    {
+        return Get<Sprite>(name);
+    }
+
+    public static void PrepareAssetBundle()
+    {
+        if (!bundle)
+        {
+            bundle = ResourceHandler.Bundles["XmasMod2025-xmas"];
+        }
+    }
+    
+    public static T Get<T>(string name) where T : Il2CppSystem.Object 
+    {
+        PrepareAssetBundle();
+        return bundle.LoadAssetAsync<T>(name).asset.Cast<T>();
+    }
+}
