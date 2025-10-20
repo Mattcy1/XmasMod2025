7659d5982c6ea78c4d6522f566e5f4249c2bdd0c
liamw <liamwolfguy@gmail.com>
2025-10-19 21:01:28 -0400

    gift counter stuff + tree outline works now

--- XmasMod2025.cs
+++ XmasMod2025.cs
@@ -50,7 +50,7 @@ public class XmasMod2025 : BloonsTD6Mod
         get => totalGifts;
     }
 
-    public override void OnNewGameModel(GameModel result)
+    public override void OnMatchStart()
     {
         if (GiftOpenerUI.instance == null)
         {
