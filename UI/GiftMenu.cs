bb5f4cc43fdf878922ab1530ef16b4aff0b16283
Mattcy <brocourtmattheo@gmail.com>
2025-10-21 11:33:54 +0200

    i hate git

--- UI/GiftMenu.cs
+++ UI/GiftMenu.cs
@@ -3,7 +3,6 @@ using BTD_Mod_Helper.Api.Components;
 using BTD_Mod_Helper.Api.Enums;
 using BTD_Mod_Helper.Extensions;
 using Il2CppAssets.Scripts.Unity.UI_New.InGame;
-using Il2CppAssets.Scripts.Unity.UI_New.Popups;
 using MelonLoader;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,11 +27,7 @@ namespace XmasMod2025.UI
 
         public void Close()
         {
-            if (gameObject)
-            {
-                gameObject.Destroy();
-            }
-
+            if (gameObject) gameObject.Destroy();
             giftText = null;
             TotalgiftText = null;
             leftArrow = null;
@@ -40,9 +35,9 @@ namespace XmasMod2025.UI
             section = null;
             slotTitle.Clear();
             slotIcon.Clear();
+            slotButton.Clear();
             page = 0;
         }
-
 
         public static string FormatNumber(long num)
         {
@@ -66,64 +61,37 @@ namespace XmasMod2025.UI
             panel.AddText(new("Text_", -700, 750, 700, 150), "GIFTS MENU", 100);
             section = panel.AddText(new("Text_", 0, 750, 700, 150), "BUFFS", 100);
 
-            rightArrow = panel.AddButton(new("Button_", 1000, 0, 200), VanillaSprites.NextArrow, new System.Action(() =>
-            {
-                page++;
-                handleArrows();
-            }));
-            leftArrow = panel.AddButton(new("Button_", -1000, 0, 200), VanillaSprites.PrevArrow, new System.Action(() =>
-            {
-                page--;
-                handleArrows();
-            }));
+            rightArrow = panel.AddButton(new("Button_", 1000, 0, 200), VanillaSprites.NextArrow, new System.Action(() => { page++; handleArrows(); }));
+            leftArrow = panel.AddButton(new("Button_", -1000, 0, 200), VanillaSprites.PrevArrow, new System.Action(() => { page--; handleArrows(); }));
 
             panel.AddImage(new("Image_", -1500, 550, 800, 400), AssetHelper.GetSprite("ChristmasPanel")).UseCustomScaling();
             panel.AddImage(new("Image_", -1500, 550, 725, 325), AssetHelper.GetSprite("ChristmasInsertPanel")).UseCustomScaling();
             panel.AddImage(new("Image_", -1775, 625, 150, 150), ModContent.GetTextureGUID<XmasMod2025>("Gift1"));
-            giftText = panel.AddText(new("Text_", -1525, 625, 700, 150), ": Gifts " + FormatNumber(XmasMod2025.Gifts), 75);
+            giftText = panel.AddText(new("Text_", -1525, 625, 700, 150), ": Gifts " + FormatNumber(XmasMod2025.Gifts), 70);
             panel.AddImage(new("Image_", -1775, 475, 150, 150), ModContent.GetTextureGUID<XmasMod2025>("PresentIcon"));
             TotalgiftText = panel.AddText(new("Text_", -1475, 475, 700, 150), ": Total Gifts " + FormatNumber(XmasMod2025.TotalGifts), 60);
 
             float w = 1900 / 3.3f;
             float h = 1300;
-            var imgCenter = panel.AddImage(new("Image_", 0, 0, w, h), AssetHelper.GetSprite("ChristmasPanelBright")).UseCustomScaling();
-            var imgRight = panel.AddImage(new("Image_", w + 50, 0, w, h), AssetHelper.GetSprite("ChristmasPanelBright")).UseCustomScaling();
-            var imgLeft = panel.AddImage(new("Image_", -w - 50, 0, w, h), AssetHelper.GetSprite("ChristmasPanelBright")).UseCustomScaling();
+            float[] xPositions = { -w - 50, 0, w + 50 };
 
-            float iw = w - 75;
-            float ih = 1225;
-            var imgInsideCenter = panel.AddImage(new("Image_", 0, 0, iw, ih), AssetHelper.GetSprite("ChristmasInsertPanelBright")).UseCustomScaling();
-            var imgInsideRight = panel.AddImage(new("Image_", w + 50, 0, iw, ih), AssetHelper.GetSprite("ChristmasInsertPanelBright")).UseCustomScaling();
-            var imgInsideLeft = panel.AddImage(new("Image_", -w - 50, 0, iw, ih), AssetHelper.GetSprite("ChristmasInsertPanelBright")).UseCustomScaling();
+            for (int i = 0; i < 3; i++)
+            {
+                var imgBright = panel.AddImage(new("Image_", xPositions[i], 0, w, h), AssetHelper.GetSprite("ChristmasPanelBright")).UseCustomScaling();
+                var imgInside = panel.AddImage(new("Image_", xPositions[i], 0, w - 75, 1225), AssetHelper.GetSprite("ChristmasInsertPanelBright")).UseCustomScaling();
+                imgInside.Image.color = new Color(0.8f, 0.8f, 0.8f);
 
-            Color color = new(0.8f, 0.8f, 0.8f);
-            imgInsideCenter.Image.color = color;
-            imgInsideRight.Image.color = color;
-            imgInsideLeft.Image.color = color;
+                slotTitle.Add(imgInside.AddText(new("Text_", 0, 550, 500, 150), "Buff", 80));
+                imgInside.AddText(new("Text_", 0, 150, 500, 150), "―――――", 80);
+                imgInside.AddImage(new("Image_", 0, 350, 500, 500), VanillaSprites.BloonariusIcon);
+                imgInside.AddText(new("Text_", 0, 0, 300, 300), "Description: Spawns bloonarius idk or do smth else", 50).Text.EnableAutoSizing(true);
+                imgInside.AddText(new("Text_", 0, -150, 500, 150), "―――――", 80);
+                imgInside.AddText(new("Text_", 0, -225, 400, 150), "Price: 500 Gifts", 50);
 
-            slotTitle.Add(imgInsideCenter.AddText(new("Text_", 0, 550, 500, 150), "Buff", 80));
-            slotTitle.Add(imgInsideRight.AddText(new("Text_", 0, 550, 500, 150), "Buff", 80));
-            slotTitle.Add(imgInsideLeft.AddText(new("Text_", 0, 550, 500, 150), "Buff", 80));
-
-            slotIcon.Add(imgInsideCenter.AddImage(new("Image_", 0, 350, 500, 500), VanillaSprites.BloonariusIcon));
-            slotIcon.Add(imgInsideRight.AddImage(new("Image_", 0, 350, 500, 500), VanillaSprites.BloonariusIcon));
-            slotIcon.Add(imgInsideLeft.AddImage(new("Image_", 0, 350, 500, 500), VanillaSprites.BloonariusIcon));
-
-            slotTitle.Add(imgInsideCenter.AddText(new("Text_", 0, 150, 500, 150), "―――――", 80));
-            slotTitle.Add(imgInsideRight.AddText(new("Text_", 0, 150, 500, 150), "―――――", 80));
-            slotTitle.Add(imgInsideLeft.AddText(new("Text_", 0, 150, 500, 150), "―――――", 80));
-
-            slotTitle.Add(imgInsideCenter.AddText(new("Text_", 0, 0, 400, 300), "Description: Spawns bloonarius idk or do smth else", 50));
-            slotTitle.Add(imgInsideRight.AddText(new("Text_", 0, 0, 400, 300), "Description: Spawns bloonarius idk or do smth else", 50));
-            slotTitle.Add(imgInsideLeft.AddText(new("Text_", 0, 0, 400, 300), "Description: Spawns bloonarius idk or do smth else", 50));
-
-            slotTitle.Add(imgInsideCenter.AddText(new("Text_", 0, -150, 500, 150), "―――――", 80));
-            slotTitle.Add(imgInsideRight.AddText(new("Text_", 0, -150, 500, 150), "―――――", 80));
-            slotTitle.Add(imgInsideLeft.AddText(new("Text_", 0, -150, 500, 150), "―――――", 80));
-
-            slotTitle.Add(imgInsideCenter.AddText(new("Text_", 0, -225, 400, 150), "Price: 500 Gifts", 50));
-            slotTitle.Add(imgInsideRight.AddText(new("Text_", 0, -225, 400, 150), "Price: 500 Gifts", 50));
-            slotTitle.Add(imgInsideLeft.AddText(new("Text_", 0, -225, 400, 150), "Price: 500 Gifts", 50));
+                var buyBtn = imgInside.AddButton(new("Button_", 0, -350, 400, 150), VanillaSprites.GreenBtnLong, new System.Action(() => { }));
+                buyBtn.AddText(new("Text_", 0, 0, 400, 150), "Buy", 60);
+                slotButton.Add(buyBtn);
+            }
 
             var Exit = panel.AddButton(new("Button_", 1000, 700, 200), VanillaSprites.RedBtnSquareSmall, new System.Action(() => instance.Close()));
             Exit.AddText(new("Text_", 0, 0, 150), "X", 100);
@@ -147,20 +115,29 @@ namespace XmasMod2025.UI
             {
                 case 0:
                     section.SetText("Buffs");
-                    slotTitle[0].SetText("Buff 1");
-                    slotTitle[1].SetText("Buff 2");
-                    slotTitle[2].SetText("Buff 0");
+                    if (slotTitle.Count >= 3)
+                    {
+                        slotTitle[0].SetText("Buff 1");
+                        slotTitle[1].SetText("Buff 2");
+                        slotTitle[2].SetText("Buff 3");
+                    }
                     break;
                 case 1:
-                    section.SetText("Yet To Come");
+                    section.SetText("WIP");
+                    if (slotTitle.Count >= 3)
+                    {
+                        slotTitle[0].SetText("WIP 1");
+                        slotTitle[1].SetText("WIP 2");
+                        slotTitle[2].SetText("WIP 3");
+                    }
                     break;
             }
         }
 
         public static void UpdateText()
         {
-            if (giftText != null) giftText.SetText(": Gifts " + FormatNumber(XmasMod2025.Gifts));
-            if (TotalgiftText != null) TotalgiftText.SetText(": Total Gifts " + FormatNumber(XmasMod2025.TotalGifts));
+            giftText?.SetText(": Gifts " + FormatNumber(XmasMod2025.Gifts));
+            TotalgiftText?.SetText(": Total Gifts " + FormatNumber(XmasMod2025.TotalGifts));
         }
     }
 
