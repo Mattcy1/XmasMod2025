ce160f1cf7530bd8df329b68067fa1fa3b3230c4
liamw <liamwolfguy@gmail.com>
2025-12-13 23:33:13 -0500

    040 cart finished + cart display

--- Towers/Upgrades/Elf/ElfMiddlePath.cs
+++ Towers/Upgrades/Elf/ElfMiddlePath.cs
@@ -22,10 +22,15 @@ using Il2CppInterop.Runtime.InteropTypes.Arrays;
 using System;
 using System.Linq;
 using BTD_Mod_Helper;
+using Il2CppAssets.Scripts.Models;
+using Il2CppAssets.Scripts.Models.Map;
+using Il2CppAssets.Scripts.Simulation.Input;
+using Il2CppAssets.Scripts.Simulation.Towers.Projectiles.Behaviors;
 using Il2CppAssets.Scripts.Simulation.Track;
 using Il2CppAssets.Scripts.Unity.Bridge;
 using MelonLoader;
 using UnityEngine;
+using Vector2 = Il2CppAssets.Scripts.Simulation.SMath.Vector2;
 
 namespace XmasMod2025.Towers.Upgrades;
 
@@ -35,7 +40,7 @@ public class BuildersSpirit : ChristmasUpgrade<ElfMonkey>
     {
         List<TowerFilterModel> filters =
         [
-            new FilterInBaseTowerIdModel("SubTowerFilterModel_BuildersSpirit", new(["Sentry", mod.IDPrefix + "-ElfToy"]))
+            new FilterInBaseTowerIdModel("SubTowerFilterModel_BuildersSpirit", new(["Sentry", TowerID<ToyMorter.ToyMorterTower>(), TowerID<ToyCart.ToyCartTower>()]))
         ];
 
         var rateSupportModel = new RateSupportModel("RateSupportModel_BuildersSpirit", 0.85f, true, "Rate:BuildersSpirit", false, 1, new Il2CppReferenceArray<TowerFilterModel>(filters.ToArray()), "", "");
@@ -59,11 +64,6 @@ public class BuildersAtmosphere : ChristmasUpgrade<ElfMonkey>
 {
     public override void ApplyUpgrade(TowerModel towerModel)
     {
-        List<TowerFilterModel> filters =
-        [
-            new FilterInBaseTowerIdModel("SubTowerFilterModel_BuildersSpirit", new(["Sentry", GetTowerModel<ToyMorter.ToyMorterTower>().baseId]))
-        ];
-
         var rateSupportModel = towerModel.GetBehavior<RateSupportModel>("RateSupportModel_BuildersSpirit");
         var rangeSupportModel = towerModel.GetBehavior<RangeSupportModel>("RangeSupportModel_BuildersSpirit");
 
@@ -148,98 +148,179 @@ public class ToyMorter : ChristmasUpgrade<ElfMonkey>
 
 public class ToyCart : ModUpgrade<ElfMonkey>
 {
-    public static Dictionary<ObjectId, Tower> TowerForProjectile = new Dictionary<ObjectId, Tower>();
+    public static Dictionary<Projectile, Tower> TowerForProjectile = new Dictionary<Projectile, Tower>();
+    public static Dictionary<Tower, Projectile> ProjectileForTower = new Dictionary<Tower, Projectile>();
     
-    public class ToyCartTower : ModSubTower
+    public class ToyCartTower : ModTower
     {
         public override void ModifyBaseTowerModel(TowerModel towerModel)
         {
+            towerModel.areaTypes = new Il2CppStructArray<AreaType>([
+                AreaType.ice,
+                AreaType.land,
+                AreaType.removable,
+                AreaType.track,
+                AreaType.water,
+                AreaType.unplaceable,
+                AreaType.water
+            ]);
+
+            towerModel.IncreaseRange(15);
+
+            var weapon = towerModel.GetWeapon();
+            var projectile = weapon.projectile;
+
+            weapon.rate = 0.05f;
+            projectile.pierce = 0;
         }
 
         public override TowerSet TowerSet => GetTowerSet<XmasTowerSet>();
         public override string BaseTower => TowerType.DartMonkey;
+        public override int Cost => 0;
+
+        public override string DisplayName => "Toy Cart";
     }
 
     public override void ApplyUpgrade(TowerModel towerModel)
     {
         var newAtk = towerModel.GetAttackModel().Duplicate();
         newAtk.fireWithoutTarget = true;
-        var newWep = newAtk.weapons[0];
-        newWep.SetEmission(new EmissionAtClosestPathSegmentModel("EmissionAtClosestPathSegmentModel_", 1, 0, new Il2CppReferenceArray<EmissionBehaviorModel>(0)));
-        newWep.projectile.AddBehavior(new TravelAlongPathModel("TravelAlongPathModel_", 30, 9999, true, false, 0));
-        newWep.projectile.RemoveBehavior<TravelStraitModel>();
-        newWep.projectile.id = "ToyCart_Low";
-        newWep.projectile.pierce = Single.MaxValue;
-        newWep.projectile.canCollideWithBloons = false;
-        newWep.fireWithoutTarget = true;
+        var weapon = newAtk.weapons[0];
+        var projectile = weapon.projectile;
+        weapon.SetEmission(new EmissionAtClosestPathSegmentModel("EmissionAtClosestPathSegmentModel_", 1, 0, new Il2CppReferenceArray<EmissionBehaviorModel>(0)));
+        projectile.AddBehavior(new TravelAlongPathModel("TravelAlongPathModel_", 30, 9999, true, false, 0));
+        projectile.RemoveBehavior<TravelStraitModel>();
+        projectile.id = "ToyCart_Low";
+        projectile.pierce = Single.MaxValue;
+        projectile.canCollideWithBloons = false;
+        projectile.GetDamageModel().damage = 3;
+        projectile.ApplyDisplay<ToyCartProjectile>();
+        projectile.RemoveBehavior<RandomDisplayModel>();
+        weapon.fireWithoutTarget = true;
+        weapon.rate = 5;
+        
         
         towerModel.AddBehavior(newAtk);
+    }
+
+    public class ToyCartProjectile : ModCustomDisplay
+    {
+        public override string AssetBundleName => "xmas";
+        public override string PrefabName => "ToyCartT1";
+
+        [RegisterTypeInIl2Cpp]
+        public class SimpleRotation : MonoBehaviour // NK why must you set every other axis to 0 with your component
+        {
+            public Vector3 axis = new(0, 0, 1); // Multiplier for x, y, z
+
+            public float rotationSpeed = 5;// Degrees/Update
+            
+            private void FixedUpdate() // 20 fps
+            {
+                transform.Rotate(axis, 5);
+            }
+        }
+        
+        public override void ModifyDisplayNode(UnityDisplayNode node)
+        {
+            foreach (var renderer in node.GetMeshRenderers())
+            {
+                if (renderer.name.StartsWith("Wheel_"))
+                {
+                    renderer.transform.rotation = Quaternion.Euler(0, 90, 0);
+                    var rot = renderer.gameObject.AddComponent<SimpleRotation>();
+                }
+                
+                renderer.ApplyOutlineShader();
+                renderer.SetOutlineColor(new Color32(59, 35, 13, 255));
+            }
+        }
+    }
+    
+    // Credits to grahamkracker https://github.com/GrahamKracker/BloonsClicker/blob/a2bc8512d11d99bfdd2394b64c030e9f7e18cb90/Main.cs#L127
+    [HarmonyPatch(typeof(TowerInventory), nameof(TowerInventory.GetTowerInventoryCount))]
+    [HarmonyPrefix]
+    static void TowerInventory_GetTowerInventoryCount(TowerInventory __instance, TowerModel def)
+    {
+        if (def.baseId == GetInstance<ToyCartTower>().Id && !__instance.towerCounts.TryGetValue(def.baseId, out _))
+        {
+            __instance.towerCounts[def.baseId] = -1;
+        }
     }
 
     [HarmonyPatch(typeof(Projectile), nameof(Projectile.Initialise))]
     private static class Projectile_Initialize
     {
-        public static int ToyCartCounter = 0;
+        public static Projectile LastProjectile;
+        public static uint ToyCartCounter = 0;
         public static void Postfix(Projectile __instance)
         {
             if (__instance.projectileModel.id == "ToyCart_Low")
             {
-                //ModHelper.Log<XmasMod2025>("Create toy cart tower");
+                ModHelper.Log<XmasMod2025>("Create toy cart tower");
                 
-                ObjectId towerId = ObjectId.FromString("ToyCartTower" + ToyCartCounter++);
-                InGame.instance.bridge.CreateTowerAt(__instance.Position.ToUnity(), GetTowerModel<ToyCartTower>(), towerId, false,new Action<bool>(suc =>
-                {
-                    if (suc)
-                    {
-                        TowerForProjectile.Add(__instance.Id, InGame.instance.bridge.GetTowerFromId(towerId));
-                    }
-                }));
+                ObjectId towerId = ObjectId.Create(9999 + ToyCartCounter++);
+                LastProjectile = __instance;
+                InGame.instance.bridge.CreateTowerAt(__instance.Position.ToUnity(), GetTowerModel<ToyCartTower>(), towerId, false,new Action<bool>(suc => {}), true, true, false, 0, false);
             }
         }
-    }[HarmonyPatch(typeof(Map), nameof(Map.ZoneAllowsPlacement))]
-    private static class Map_ZoneAllowsPlacement
+    }
+    [HarmonyPatch(typeof(Projectile), nameof(Projectile.OnDestroy))]
+    private static class Projectile_OnDestroy
     {
-        public static int ToyCartCounter = 0;
-        public static bool Prefix(Map __instance, IPlaceableEntity pe, ref bool __result)
+        public static void Postfix(Projectile __instance)
         {
-            Tower? tower = pe.TryCast<Tower>();
-            if (tower == null)
+            if (TowerForProjectile.ContainsKey(__instance))
             {
-                return true;
+                ProjectileForTower.Remove(TowerForProjectile[__instance]);
+                TowerForProjectile[__instance].Destroy();
+                TowerForProjectile.Remove(__instance);
             }
-            
-            if (tower.towerModel.baseId == TowerID<ToyCartTower>())
+        }
+    }
+    [HarmonyPatch(typeof(Tower), nameof(Il2CppAssets.Scripts.Simulation.Towers.Tower.OnSold))]
+    [HarmonyPatch(typeof(Tower), nameof(Il2CppAssets.Scripts.Simulation.Towers.Tower.OnDestroy))]
+    private static class Tower_OnDestroy
+    {
+        public static void Postfix(Tower __instance)
+        {
+            if (ProjectileForTower.ContainsKey(__instance))
             {
-                __result = true;
-                return false;
+                TowerForProjectile.Remove(ProjectileForTower[__instance]);
+                ProjectileForTower[__instance].Destroy();
+                ProjectileForTower.Remove(__instance);
             }
-            
-            return true;
         }
     }
 
-    [HarmonyPatch(typeof(InGame), nameof(InGame.Update))]
-    private static class InGame_Update
+    [HarmonyPatch(typeof(Tower), nameof(Il2CppAssets.Scripts.Simulation.Towers.Tower.Initialise))]
+    private static class Tower_Initialize
     {
-        public static void Postfix(InGame __instance)
+        public static void Postfix(Tower __instance, Model modelToUse)
         {
-            if (!__instance.IsInGame())
+            if (modelToUse.Cast<TowerModel>().baseId == TowerID<ToyCartTower>())
             {
-                return;
+                TowerForProjectile.Add(Projectile_Initialize.LastProjectile, __instance);
             }
-            
-            var validProjectiles = __instance.GetProjectiles().Where(proj => TowerForProjectile.ContainsKey(proj.Id));
-            foreach (var projectile in validProjectiles)
-            {
-                var tower = TowerForProjectile[projectile.Id];
-                
-                tower.PositionTower(projectile.Position.ToVector2());
-                tower.Rotation = projectile.Rotation;
-            }
+        }
+    }
+
+    [HarmonyPatch(typeof(TravelAlongPath), nameof(TravelAlongPath.Process))]
+    private static class TravelAlongPath_Process
+    {
+        public static void Postfix(TravelAlongPath __instance)
+        {
+            var projectile = __instance.projectile;
+            var tower = TowerForProjectile[projectile];
+            tower.PositionTower(projectile.Position.ToVector2());
+            tower.Rotation = projectile.Rotation;
         }
     }
 
     public override int Path => Middle;
     public override int Tier => 4;
     public override int Cost => 225;
+
+    public override string Description =>
+        "Elf monkey can now create carts which run over bloons for <b>three</b> damage and have fast shooting turrets that only do <b>one</b> damage each shot.";
 }
