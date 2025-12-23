02c811c99f990bd9fd29419e941d38de6ec0e27e
liamw <liamwolfguy@gmail.com>
2025-12-22 19:14:40 -0500

    candy cane 500 portrait + update description and README.md

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,6 +1,4 @@
-﻿using Il2CppAssets.Scripts.Unity.UI_New.InGame.RightMenu.Powers;
-
-namespace XmasMod2025;
+﻿namespace XmasMod2025;
 
 public static class ModHelperData
 {
@@ -8,7 +6,12 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "Christmas Mod 2025";
 
-    public const string Description = "Welcome, to the second Xmas Mod\r\n\r\nIt’s based on last year’s mod, but instead of The Grinch, the antagonist is Krampus; a similar boss, but with his own unique traits. It includes custom dialogue to help bring the story to life, custom Xmas themed bloons, 2 full 5-5-5 towers, a gift shop, and much more!";
+    public const string Description = "Welcome, to the second Xmas Mod\r\n\r\nIt’s based on last year’s mod, but instead of The Grinch, the antagonist is Krampus; a similar boss, but with his own unique traits. It includes custom dialogue to help bring the story to life, custom Xmas themed bloons, 2 full 5-5-5 towers, a gift shop, and much more!\n\n" +
+                                      "Credits:\n" +
+                                      "- Popping Productions - Santa portraits from last year\n" +
+                                      "- Mattcy - Candy Cane Monkey logic and art, bottom path elf monkey & all elf monkey t1-t2 icons (except top path), bottom path and middle path logic, all boss logic & api, gift shop logic and icons, candy cane bloon, most bloon logic\n" +
+                                      "- LerpMcGerk - Elf Monkey Middle and top path icons (100, 300-500, 030-050), all elf displays, elf top path logic, boss models and icons, bloons (except candy cane bloon) and moabs textures and icons, snow moab and bloon logic, post processing effects, dialogue messages and portraits (other than Santa's portraits), custom ui panels, xmas tree model\n" +
+                                      "- ReLogic - Made the krampus boos music (FrostMoon.wav is from the game Terraria)";
 
     public const string RepoOwner = "Mattcy1";
     public const string Icon = "XmasTowerSet-Button.png";
--- README.md
+++ README.md
@@ -3,12 +3,22 @@
     <img align="left" alt="Icon" height="90" src="https://github.com/Mattcy1/XmasMod2025/blob/master/Assets/XmasTowerSet-Button.png">
     <img align="right" alt="Download" height="75" src="https://raw.githubusercontent.com/gurrenm3/BTD-Mod-Helper/master/BloonsTD6%20Mod%20Helper/Resources/DownloadBtn.png">
 </a>
-Xmas Mod 2025
+Christmas Mod 2025
 </h1>
 
 Welcome, to the second Xmas Mod
 
 It’s based on last year’s mod, but instead of The Grinch, the antagonist is Krampus; a similar boss, but with his own unique traits. It includes custom dialogue to help bring the story to life, custom Xmas themed bloons, 2 full 5-5-5 towers, a gift shop, and much more! 
-# Get ready to save Xmas once again!
+
+Get ready to save Xmas once again!
+
+## Credits: 
+- Popping Productions - Santa portraits from last year
+
+- Mattcy - Candy Cane Monkey logic and art, bottom path elf monkey & all elf monkey t1-t2 icons (except top path), bottom path and middle path logic, all boss logic & api, gift shop logic and icons, candy cane bloon, most bloon logic
+
+- LerpMcGerk - Elf Monkey Middle and top path icons (100, 300-500, 030-050), all elf displays, elf top path logic, boss models and icons, bloons (except candy cane bloon) and moabs textures and icons, snow moab and bloon logic, post processing effects, dialogue messages and portraits (other than Santa's portraits), custom ui panels, xmas tree model
+
+- ReLogic - Made the krampus boos music (FrostMoon.wav is from the game Terraria)
 
 [![Requires BTD6 Mod Helper](https://raw.githubusercontent.com/gurrenm3/BTD-Mod-Helper/master/banner.png)](https://github.com/gurrenm3/BTD-Mod-Helper#readme)
